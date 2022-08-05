9b4bf18f9d384a93b63117686d1dc1991bafcf70
Priyanshu2410 <95230102+Priyanshu2410@users.noreply.github.com>
2022-08-06 01:48:40 +0530

    Update 21.cpp

--- hackerrank/21.cpp
+++ hackerrank/21.cpp
@@ -38,3 +38,34 @@ int main() {
      aa.om();
     return 0;
 }
+/* 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Rectangle{
+    public:
+    int width;
+    int height;
+    
+    void display(){
+        cout<<width<<" "<<height<<endl;
+    }
+};
+class RectangleArea : public Rectangle{
+    public:
+    void read_input(){
+        cin>>width>>height;
+    }
+    void display(){
+        cout<<width*height<<endl;
+    }
+};
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    RectangleArea ra;
+    ra.read_input();
+    ra.Rectangle::display();//it access the base class 
+    ra.display();
+    
+    return 0;
+}  */
