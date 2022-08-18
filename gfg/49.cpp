42d66ebb9d1d067f22fba96a112456eb777238ee
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-08-18 23:41:48 +0530

    gfg

---
+++ gfg/49.cpp
@@ -0,0 +1,42 @@
+// Find minimum and maximum element in an array
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    long long min=INT_MAX,max=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+        if(a[i]<min)
+        {
+            min=a[i];
+        }
+    }
+    return make_pair(min,max);
+}
