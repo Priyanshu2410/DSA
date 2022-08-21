e6d2353eb615039f3a9095469d392ba9e956c5dd
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-08-21 22:35:55 +0530

    gfg

---
+++ gfg/72.cpp
@@ -0,0 +1,79 @@
+
+// Subarray with 0 sum
+
+//{ Driver Code Starts
+// A C++ program to find if there is a zero sum
+// subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        //Your code here
+           
+      for(int i=0;i<n;i++)
+      {
+          if(arr[i] == 0)
+          {
+              return true;
+           }
+       }
+       
+           int sum=0;
+       for(int i=0;i<n;i++){
+          sum+=arr[i];
+          
+          if(sum==0){
+              return true;
+          }
+      }
+        unordered_map<int,int> mp;
+     
+      sum=0;
+   
+       for(int i=0;i<n;i++){
+           
+           sum+=arr[i];
+           
+           mp[sum]++;
+      }
+      
+      
+      for(auto i : mp){
+          if(i.second > 1){
+              return true;
+          }
+      }
+      return false;
+    }
+};
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
