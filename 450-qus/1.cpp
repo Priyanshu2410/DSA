df4079c61f2a0c638df686ae7d70937a0a0db432
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-08-24 23:41:12 +0530

    450 qus

---
+++ 450-qus/1.cpp
@@ -0,0 +1,32 @@
+// Reverse an Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//code
+	long long a;
+	cin>>a;
+	for(long long t=0;t<a;t++)
+	{
+    	long long n;
+    
+        cin>>n;
+        long long arr[n];
+        for(long long i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        long long arr2[n];
+        for(long long i=0;i<n;i++)
+        {
+            arr2[i]=arr[n-i-1];
+        }
+        for(long long i=0;i<n;i++)
+        {
+            cout<<arr2[i]<<" ";
+        }
+        cout<<endl;
+}
+	return 0;
+}
---
+++ 450-qus/2.cpp
@@ -0,0 +1,43 @@
+// Find minimum and maximum element in an array
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+      int min = a[0];
+   int max = a[0];
+   
+   for(int i=0; i<n; i++){
+       if(max < a[i]){
+           max = a[i];
+       }
+       
+       if(min > a[i]){
+           min = a[i];
+       }
+   }
+   return make_pair(min, max);
+
+}
