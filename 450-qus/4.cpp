c0d3fabd272734c62a9f3c8dd4f1464ffdc95eb1
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-08-25 15:10:18 +0530

    450 qus

---
+++ 450-qus/4.cpp
@@ -0,0 +1,78 @@
+// Sort an array of 0s, 1s and 2s
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int A[], int n)
+  {
+        int i = 0 ,  countzero = 0 , countone = 0 ,  counttwo = 0 ;  
+    while ( i < n )  
+    {  
+        if ( A [ i ] == 0 )  
+        {  
+            countzero = countzero + 1 ;  
+        }  
+        else if ( A [ i ] == 1 )  
+        {  
+            countone = countone + 1 ;  
+        }  
+        else  
+        {  
+            counttwo = counttwo + 2 ;  
+        }  
+        i = i + 1 ;  
+    }  
+    for  ( i = 0 ; i < countzero ; i++ )  
+    {  
+        A [ i ] = 0 ;  
+    }  
+    for ( i = countzero ; i < countzero + countone ; i++ )  
+    {  
+        A [ i ] = 1 ;  
+    }  
+    for ( i = countzero + countone ; i < n ; i++ )  
+    {  
+        A [ i ] = 2 ;  
+    }  
+  }
+  
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
