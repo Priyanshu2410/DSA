405fca01ec6858b54fb5d7e659252c3e6c4ea5a4
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-08-28 23:33:36 +0530

    new450

---
+++ 450-qus/string1.cpp
@@ -0,0 +1,14 @@
+// Reverse String
+
+// class Solution {
+// public:
+//     void reverseString(vector<char>& str) {
+//         int s=0;
+//         int e=str.size()-1;
+//         int temp;
+        
+//         while(s<e) {
+//            swap(str[s++],str[e--]);
+//         } 
+//     }
+// };
---
+++ 450-qus/string2.cpp
@@ -0,0 +1,53 @@
+// Check whether a String is Palindrome or not
+
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	     for(int i=0,j=S.length()-1;i<S.length()/2;i++,j--)
+        {
+            if(S[i] != S[j])
+                {
+                    return 0;
+                }
+        
+        }
+       return 1;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
