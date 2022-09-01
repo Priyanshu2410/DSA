e51cc75854b0af2e1571e88fa7ae71a77bd8e253
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-09-01 22:16:20 +0530

    basic

---
+++ gfg/GraytoBinaryequivalent.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // function to convert a given Gray equivalent n to Binary equivalent.
+    int grayToBinary(int n)
+    {
+        
+
+
+ int res = 0;
+        while(n){
+            res^=n;
+            n>>=1;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t,n;
+    cin>>t;//testcases
+    while(t--)
+    {
+        cin>>n;//initializing n
+        
+        Solution ob;
+        //calling function grayToBinary()
+       cout<< ob.grayToBinary(n)<<endl;
+    }
+}
+
+// } Driver Code Ends
