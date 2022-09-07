c09f55eaafc80bf22ede10d7446971460bfb976e
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-09-07 22:27:21 +0530

    madium array

--- Linked List/note1.cpp
+++ Linked List/note1.cpp
@@ -1,25 +1,54 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-class Nodes{
-    public:
-        int data;
-        Nodes* next;
+class Nodes
+{
+public:
+    int data;
+    Nodes *next;
 
-        //construcutor
+    // construcutor
 
-        Nodes(int data)
-        {
-            this -> data=data;
-            this -> next=next;
-        }
+    Nodes(int data)
+    {
+        this->data = data;
+        this->next = next;
+    }
 };
 
+void insertatHead(Nodes* &head, int d)
+{
+    Nodes* temp = new Nodes(d);
+    temp->next = head;
+    head = temp;
+}
+
+void print(Nodes* &head)
+{
+    Nodes* temp = head;
+
+    while(temp != NULL )
+    {
+        cout << temp -> data <<" ";
+        temp = temp -> next;
+    }
+}
 int main()
 {
-    Nodes*  node1 = new Nodes(10);
-    cout<< node1 ->data<<endl;
-    cout<< node1 ->next<<endl;
+    // created a new node
+    Nodes* node1 = new Nodes(10);
+    //  cout<< node1 ->data<<endl;
+    // cout<< node1 ->next<<endl;
 
+    // head pointed to node
+    Nodes* head = node1;
+   print(head);
+
+    insertatHead(head, 12);
+//    print(head);
+
+    insertatHead(head, 15);
+   // print(head);
     return 0;
 }
