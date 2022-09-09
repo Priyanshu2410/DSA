9cbcd4373a6d81a8f4c632f186c8b1425ed17419
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-09-09 22:13:34 +0530

    easy array

---
+++ codechef/Practicemakesusperfect.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a[4],count=0;
+	for(int i=0;i<4;i++)
+	{   
+	    
+	    cin>>a[i];
+	    if(a[i]>=10)
+	       count++;
+	    
+	    
+	}
+	 cout<<count<<endl;
+	return 0;
+}
