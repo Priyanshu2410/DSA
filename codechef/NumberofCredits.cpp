6d627d443918d40832f16960968d7421dba8520b
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-09-09 21:41:51 +0530

    easy cpp

---
+++ codechef/NumberofCredits.cpp
@@ -0,0 +1,16 @@
+// first code
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,a,b,c;
+	cin>>n;
+	for (int i=0;i<n;i++)
+	{
+	    cin>>a>>b>>c;
+	    cout<<a*4+b*2<<endl;
+	}
+	return 0;
+}
---
+++ gfg/SpecialStack.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include<iostream>
+#include<stack>
+using namespace std;
+void push(stack<int>& s,int a);
+bool isFull(stack<int>& s,int n);
+bool isEmpty(stack<int>& s);
+int pop(stack<int>& s);
+int getMin(stack<int>& s);
+//This is the STL stack (http://quiz.geeksforgeeks.org/stack-container-adaptors-the-c-standard-template-library-stl/).
+stack<int> s;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,a;
+		cin>>n;
+		while(!isEmpty(s)){
+		    pop(s);
+		}
+		while(!isFull(s,n)){
+			cin>>a;
+			push(s,a);
+		}
+		cout<<getMin(s)<<endl;
+	}
+}
+// } Driver Code Ends
+
+void push(stack<int>& s, int a){
+// Your code goes here
+s.push(a);
+}
+
+bool isFull(stack<int>& s,int n){
+//rear == SIZE-1 && front == 0) || (rear == front-1)   
+      if(s.size()==n){
+          return true;
+          
+      }
+}
+
+bool isEmpty(stack<int>& s){
+// Your code goes here
+if(s.empty()){
+    return true;
+}
+}
+
+int pop(stack<int>& s){
+// Your code goes here
+s.pop();
+}
+
+int getMin(stack<int>& s){
+// Your code goes here
+int mini=2147483647;
+while(!s.empty()){
+int a=s.top();
+ s.pop();
+ mini=min(a,mini);
+}
+return mini;
+}
