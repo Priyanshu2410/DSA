eab6f28797195dfe18a8a5ec521ee71e53263352
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-09-09 21:50:59 +0530

    easy if-else

---
+++ codechef/Fever.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	 int n;
+    cin>>n;
+    while(n--){
+        int x;
+        cin>>x;
+    if(x<=98){
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"YES"<<endl;
+    }
+    }
+	return 0;
+}
