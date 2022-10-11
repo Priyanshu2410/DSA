f443b72a093353f13d48ec7de668fa896555e736
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-10-11 23:43:50 +0530

    linked list

---
+++ Linked List/doublylinkedlist.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int d){
+        this->data=d;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+
+void print(Node* head)
+{
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+int getlenght(Node* head)
+{
+    int len=0;
+     Node* temp=head;
+    while(temp!=NULL){
+        // cout<<temp->data<<" ";
+        len++;
+        temp=temp->next;
+    }
+    // cout<<endl;
+    return len;
+}
+void insertAthead(Node* &head,int d)
+{
+    Node* temp=new Node(d);
+    temp ->next=head;
+    head ->prev=temp;
+    head=temp;
+
+}
+
+void insertAttail(Node* &tail,int d)
+{
+    Node* temp=new Node(d);
+    tail->next=temp;
+    temp->prev=tail;
+    tail=temp;
+}
+
+int main()
+{
+    Node* node1 = new Node(10);
+    Node* head =node1;
+    Node* tail=node1;
+    print(head);
+    // cout<<getlenght(head)<<endl;
+    insertAthead(head,11);
+    print(head);
+    insertAthead(head,24);
+    print(head);
+    insertAthead(head,21);
+    print(head);
+    insertAttail(tail,15);
+    print(head);
+    return 0;
+}
