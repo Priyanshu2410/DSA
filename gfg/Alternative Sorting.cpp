af8b8511669728dc0ec58d7a321ed46b3f4d5881
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-11-04 17:29:02 +0530

    sort good

---
+++ gfg/Alternative Sorting.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+
+	public:
+	vector<int> alternateSort(int arr[], int N)
+	{
+	    // Your code goes here
+	    sort(arr,arr+N);
+	    vector<int> ans(N);
+	    int i=0,
+	    j=N-1,
+	    a=0;
+	    while(a<N){
+	        ans[a++] = arr[j--];
+	        if(a<N) 
+	        {
+	            ans[a++] = arr[i++];
+	        }
+	    }
+	    return ans;
+	}
+
+};
+	
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n;
+       	
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+       
+
+        Solution ob;
+        vector<int> ans = ob.alternateSort(a, n);
+        for(auto i:ans)
+        	cout << i << " ";
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
