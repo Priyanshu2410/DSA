95e0147cc2c90942d30c22ce7216de9eecbee000
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-11-13 14:20:52 +0530

    sort stack

---
+++ gfg/Sort a stack.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void SortedStack :: sort()
+{
+   stack<int> St;
+   while(!s.empty()){
+       int tmp = s.top();
+       s.pop();
+       
+       while(!St.empty() && St.top() > tmp){
+           s.push(St.top());
+          St.pop();
+       }
+       St.push(tmp);
+   }
+   s =St;
+}
