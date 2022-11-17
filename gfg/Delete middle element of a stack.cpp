fa2409d84cc2fc537d58a4cd44c20c291afba001
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-11-17 21:56:09 +0530

    del mid

---
+++ gfg/Delete middle element of a stack.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        stack<int>st;
+        int x=s.size();
+        for(int i=0;i<x/2;i++)
+        {
+            st.push(s.top());
+            s.pop();
+        }
+        s.pop();
+        while(st.empty()==false)
+        {
+            s.push(st.top());
+            st.pop();
+        } 
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int sizeOfStack;
+        cin>>sizeOfStack;
+        
+        stack<int> myStack;
+        
+        for(int i=0;i<sizeOfStack;i++)
+        {
+            int x;
+            cin>>x;
+            myStack.push(x);    
+        }
+
+            Solution ob;
+            ob.deleteMid(myStack,myStack.size());
+            while(!myStack.empty())
+            {
+                cout<<myStack.top()<<" ";
+                myStack.pop();
+            }
+        cout<<endl;
+    }   
+    return 0;
+}
+
+// } Driver Code Ends
