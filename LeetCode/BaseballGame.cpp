103a737666e9ac98d049771ca7c6233bb07116d7
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-11-25 19:36:11 +0530

    stack easy

---
+++ LeetCode/BaseballGame.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        vector<int> v;
+        for (string ch : ops) {
+            if (ch == "C")
+                v.pop_back();
+            else if (ch == "D")
+                v.push_back(v.back() * 2);
+            else if (ch == "+")
+                v.push_back(v.back() + v[v.size() - 2]);
+            else
+                v.push_back(stoi(ch));
+        }
+        
+        return accumulate(v.begin(), v.end(), 0);
+    }
+};
