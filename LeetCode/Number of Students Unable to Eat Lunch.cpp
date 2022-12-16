7fb36bb0ee8e6959de0839c703c28c8eb057612d
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2022-12-16 17:44:10 +0530

    stack easy

---
+++ LeetCode/Number of Students Unable to Eat Lunch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+   int count=0;
+        while(true){
+            if(students[0]==sandwiches[0]){
+                students.erase(students.begin());
+                sandwiches.erase(sandwiches.begin());
+                count=0;
+            }else{
+                students.push_back(students[0]);
+                students.erase(students.begin());
+                count++;
+            }
+            if(students.size()==0)
+                return 0;
+            if(count==students.size())
+                break;
+        }
+        return count;
+    }
+};
