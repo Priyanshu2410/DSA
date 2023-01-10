308a57fe4ac732c59ed8e28de2535b006997bcda
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-01-10 16:23:01 +0530

    dp

---
+++ Dynamic Programing/topdownaproc.cpp
@@ -0,0 +1,36 @@
+// fibonacci series
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int fib(int n,vector<int> &dp)
+{
+    if(n<=1)
+    {
+        return n;
+    }
+    if(dp[n]!=-1)
+    {
+        return dp[n];
+    }
+
+    dp[n]=fib(n-1,dp)+fib(n-2,dp);
+    return dp[n];
+}
+
+int main()
+{
+    int n;
+    cout<<"ente your number"<<endl;
+    cin>>n;
+    vector<int>dp(n+1);
+    for(int i=0;i<=n;i++)
+    {
+        dp[i] = (-1);
+    }
+
+    cout<<fib(n,dp)<<endl;
+
+    return 0;
+}
