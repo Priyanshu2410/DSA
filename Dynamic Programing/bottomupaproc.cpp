6486828256a24f1fcc6bd10cb5f83260bec45f07
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-01-10 16:47:23 +0530

    dp

---
+++ Dynamic Programing/bottomupaproc.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter your number"<<endl;
+    cin>>n;
+    vector<int>dp(n+1);
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    cout<<dp[n]<<endl;
+
+    return 0;
+}
