3b56e7276fca2df2843c7405940b3820e99f0c26
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-06-21 22:06:17 +0530

    array good leetcode

---
+++ AtoZ/Array/Easy/Check if Array Is Sorted and Rotated_leetcode.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int cnt=0;
+        
+        if(nums.size()<=2)
+            return true;
+        
+        for(int i=1; i<nums.size(); i++)
+            // detect order broken 
+            if(nums[i]<nums[i-1]){
+                cnt++;
+                if(cnt>1)
+                    return false;   
+                if(nums[i]>nums[0] || nums[nums.size()-1]>nums[0])
+                    return false;
+            }
+        
+        
+        return true;
+
+    }
+};
