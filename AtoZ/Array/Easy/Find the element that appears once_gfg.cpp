2bb0af9206b68e33c7bf84a94b09a7c07b3e0dc1
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-06-28 23:41:55 +0530

    array easy

---
+++ AtoZ/Array/Easy/Find the element that appears once_gfg.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int search(int A[], int N){
+	   
+	    int xoar =0;
+	    for(int i=0;i<N;i++)
+	    {
+	         xoar = xoar ^ A[i];
+	    }
+	    return xoar;
+	    
+	}
+};
+
+//{ Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t,len;
+    cin>>t;
+    while(t--)
+    {
+        cin>>len;
+        int arr[len];
+        for(int i=0;i<len;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.search(arr, len)<<'\n';
+    }
+    return 0;
+}
+
+// } Driver Code Ends
