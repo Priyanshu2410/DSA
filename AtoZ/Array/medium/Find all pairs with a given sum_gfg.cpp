ab3a550127e702c9c2d5cfc48e4b2f3f0b251d37
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-06-29 23:45:01 +0530

    gfg arr good

---
+++ AtoZ/Array/medium/Find all pairs with a given sum_gfg.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
+    {
+        vector<pair<int,int>> ans;
+        sort(A,A+N);
+        sort(B,B+M);
+        reverse(B,B+M);
+        
+        int s1=0,s2=0;
+        while(s1<N && s2<M)
+        {
+            if(A[s1]+B[s2]==X)
+            {
+                ans.push_back({A[s1],B[s2]});
+                s1++;
+                s2++;
+            }
+            else if(A[s1]+B[s2]<X)
+            {
+                s1++;
+            }
+            else
+            {
+                s2++;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    int N, M, X;
+	    cin >> N >> M >> X;
+        int A[N], B[M];
+        
+	    for(int i = 0;i<N;i++)
+	        cin >> A[i];
+	    for(int i = 0;i<M;i++)
+	        cin >> B[i];
+	        
+	   Solution ob;
+	   vector<pair<int,int>> vp = ob.allPairs(A, B, N, M, X);
+	   int sz = vp.size();
+        if(sz==0)
+        cout<<-1<<endl;
+        else{
+            for(int i=0;i<sz;i++){
+                if(i==0)
+                cout<<vp[i].first<<" "<<vp[i].second;
+                else
+                cout<<", "<<vp[i].first<<" "<<vp[i].second;
+            }
+            cout<<endl;
+        }
+	}
+	return 0;
+}
+// } Driver Code Ends
