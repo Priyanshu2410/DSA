dfaffbf989e2d83d6310864bea8952be052f1bbd
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-07-14 22:56:03 +0530

    coding ninjas binary search

---
+++ AtoZ/BinarySearch/Easy/Find Minimum in Rotated Sorted Array_CN.cpp
@@ -0,0 +1,30 @@
+int findMin(vector<int>& arr)
+{
+	 int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+      
+        if (arr[low] <= arr[high]) {
+            ans = min(ans, arr[low]);
+            break;
+        }
+
+        if (arr[low] <= arr[mid]) {
+         
+            ans = min(ans, arr[low]);
+
+            
+            low = mid + 1;
+        }
+        else { 
+
+            
+            ans = min(ans, arr[mid]);
+
+            
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
