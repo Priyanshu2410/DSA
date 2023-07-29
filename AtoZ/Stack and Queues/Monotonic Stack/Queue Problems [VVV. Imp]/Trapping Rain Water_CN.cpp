d8b29d21b92be319ce1145ac24fd749ee7c09870
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-07-29 22:10:40 +0530

    stack easy not use stack

---
+++ AtoZ/Stack and Queues/Monotonic Stack/Queue Problems [VVV. Imp]/Trapping Rain Water_CN.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+long long getTrappedWater(long long* arr, int n) {    
+  if (n <= 2) {
+        return 0; 
+    }
+    long long trappedWater = 0;
+    int left = 0;
+    int right = n - 1;
+    long long leftMax = 0;
+    long long rightMax = 0;
+
+    while (left <= right) {
+        if (arr[left] <= arr[right]) {
+            if (arr[left] > leftMax) {
+               leftMax = arr[left];
+            } else {
+                trappedWater += leftMax - arr[left];
+            }
+            left++;
+        } else {
+            if (arr[right] > rightMax) {
+                rightMax = arr[right];
+            } else {
+                trappedWater += rightMax - arr[right];
+            }
+            right--;
+        }
+    }
+    return trappedWater;
+}
