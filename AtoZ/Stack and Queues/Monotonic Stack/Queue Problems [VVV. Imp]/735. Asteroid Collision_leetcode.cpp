53cab842e7ac53700adf70429ecf5bea63085b3e
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-07-30 23:55:05 +0530

    stack leetcode  stack good

---
+++ AtoZ/Stack and Queues/Monotonic Stack/Queue Problems [VVV. Imp]/735. Asteroid Collision_leetcode.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+         vector<int> ans;
+    stack<int> st;
+
+    int n = asteroids.size();
+
+    for (int i = 0; i < n; i++) {
+        int a = asteroids[i];
+
+        if (a > 0) {
+            st.push(a);
+        } else {
+            while (!st.empty() && st.top() > 0 && st.top() < abs(a)) {
+                st.pop();
+            }
+
+            if (st.empty() || st.top() < 0 || st.top() < abs(a)) {
+                st.push(a);
+            } else if (st.top() == abs(a)) {
+                st.pop();
+            }
+        }
+    }
+
+    while (!st.empty()) {
+        ans.insert(ans.begin(), st.top());
+        st.pop();
+    }
+
+    return ans;
+    }
+};
