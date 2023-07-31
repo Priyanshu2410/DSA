e7a405a1f3c3b02aca0caaff62bb0562aef3363e
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-07-31 19:15:11 +0530

    leetcode  stack good

---
+++ AtoZ/Stack and Queues/Monotonic Stack/Queue Problems [VVV. Imp]/2104. Sum of Subarray Ranges_leetcode.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+         long long int ans=0;
+        int n=nums.size();
+        for (int i=0;i<n;i++)
+        {
+            int maxi=nums[i];
+            int mini=nums[i];
+            // cout<<maxi<<" "<<mini<<endl;
+            for (int j=i+1;j<n;j++)
+            {
+                if (nums[j]>maxi)
+                    maxi=nums[j];
+                if (nums[j]<mini)
+                    mini=nums[j];
+                
+                // cout<<maxi<<" "<<mini<<endl;
+                ans+=(maxi-mini);
+                
+            }
+        }
+        return ans;
+    }
+};
