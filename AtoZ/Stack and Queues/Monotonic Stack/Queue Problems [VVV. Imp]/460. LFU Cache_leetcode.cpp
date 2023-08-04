c620c194025b754473bbc70d1159f32b4deb83a5
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-08-04 15:48:44 +0530

    Lfu DS v.good m.imp

---
+++ AtoZ/Stack and Queues/Monotonic Stack/Queue Problems [VVV. Imp]/460. LFU Cache_leetcode.cpp
@@ -0,0 +1,83 @@
+class LFUCache {
+private:
+    int cap;
+    int size;
+    unordered_map<int, list<vector<int>>::iterator> mp; 
+    map<int, list<vector<int>>> freq;
+    
+public:
+    LFUCache(int capacity) {
+        cap = capacity;
+        size = 0;
+    }
+    
+    void makeMostFrequentlyUsed(int key) {
+        auto &vec = *(mp[key]);
+        
+        int value = vec[1];
+        
+        int f     = vec[2];
+        
+        freq[f].erase(mp[key]);
+        
+        if(freq[f].empty())
+            freq.erase(f);
+        
+        f++;
+        
+        freq[f].push_front(vector<int>({key, value, f}));
+        
+        mp[key] = freq[f].begin();
+    }
+    
+    int get(int key) {
+        if(mp.find(key) == mp.end())
+            return -1;
+        
+        auto &vec = (*(mp[key]));
+        
+        int value = vec[1];
+        
+        makeMostFrequentlyUsed(key);
+        
+        return value;
+    }
+    
+    void put(int key, int value) {
+        if(cap == 0)
+            return;
+        
+        if(mp.find(key) != mp.end()) {
+            auto &vec = (*(mp[key]));
+            vec[1] = value;
+            makeMostFrequentlyUsed(key);
+        }
+        else if(size < cap) {
+            size++;
+            freq[1].push_front(vector<int>({key, value, 1}));
+            mp[key] = freq[1].begin();
+        }
+        else { 
+            
+            auto &list134 = freq.begin()->second;
+            
+            int key_delete = (list134.back())[0]; 
+            
+            list134.pop_back();
+            
+            if(list134.empty())
+                freq.erase(freq.begin()->first);
+            
+            freq[1].push_front(vector<int>({key, value, 1}));
+            
+            mp.erase(key_delete);
+            mp[key] = freq[1].begin();
+        }
+    }
+};
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache* obj = new LFUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
