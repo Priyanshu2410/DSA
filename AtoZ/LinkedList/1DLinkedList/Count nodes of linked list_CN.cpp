2ec6b9494bd69f1f9e5c9e50e032acc20485ac50
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-08-06 18:58:34 +0530

    coding ninjas linked list easy

---
+++ AtoZ/LinkedList/1DLinkedList/Count nodes of linked list_CN.cpp
@@ -0,0 +1,42 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+int length(Node *head)
+{
+	   int cnt=1;
+
+    if(head==NULL)return 0;
+
+    while(head->next!=NULL){
+        cnt++;
+        head=head->next;
+    }
+
+    return cnt;
+}
