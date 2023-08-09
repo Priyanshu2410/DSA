61127db5cf5b9d2f0d3c1b52972422909c0b093a
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-08-10 00:43:12 +0530

    coding ninjas LL good easy

---
+++ AtoZ/LinkedList/Medium Problems of LL/Reverse Linked List_CN.cpp
@@ -0,0 +1,40 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+        
+
+*****************************************************************/
+
+Node* reverseLinkedList(Node *head)
+{
+    Node *newHead = NULL;
+        while (head != NULL) {
+            Node *next = head->next;
+            head->next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+}
