f12be8def29c8b1d02009acbc0d337fc96bfe1a4
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-09-10 23:35:47 +0530

    D linked list coding ninjas

---
+++ AtoZ/LinkedList/Medium Problems of DLL/Remove duplicates from a sorted Doubly Linked List_CN.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition of doubly linked list:
+ * class Node {
+ * public:
+ *      int data;
+ *      Node *prev;
+ *      Node *next;
+ *      Node() {
+ *          this->data = 0;
+ *          this->prev = NULL;
+ *          this->next = NULL;
+ *      }
+ *      Node(int data) {
+ *          this->data = data;
+ *          this->prev = NULL;
+ *          this->next = NULL;
+ *      }
+ *      Node (int data, Node *next, Node *prev) {
+ *          this->data = data;
+ *          this->prev = prev;
+ *          this->next = next;
+ *      }
+ * };
+ *
+ *************************************************************************/
+
+Node * removeDuplicates(Node *head)
+{
+    // Write your code here
+    Node* temp=head;
+
+    while(temp!=NULL){
+
+        if((temp->next!=NULL)&&(temp->data==temp->next->data)){
+
+            Node* remove=temp->next;
+
+            temp->next=temp->next->next;
+
+            delete remove;
+
+        }else{
+
+            temp=temp->next;
+
+        }
+
+    }
+
+    return head;
+}
