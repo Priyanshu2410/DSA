50ef84592a7ef652267ba072b05e3aa7cca0d7ac
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-09-15 23:48:02 +0530

    binary tree both

---
+++ AtoZ/Binary Trees/Medium/Check if the Binary tree is height-_CN_leet.cpp
@@ -0,0 +1,46 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode
+    {
+    public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val)
+        {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+int dfsHeight (TreeNode<int>* root) {
+
+        if (root == NULL) return 0;
+        
+        int leftHeight = dfsHeight (root -> left);
+
+        if (leftHeight == -1) 
+            return -1;
+        
+        int rightHeight = dfsHeight (root -> right);
+
+        if (rightHeight == -1) 
+            return -1;
+        
+        if (abs(leftHeight - rightHeight) > 1)  
+            return -1;
+
+        return max (leftHeight, rightHeight) + 1;
+    }
+bool isBalancedBT(TreeNode<int>* root){
+    // Write your code here.
+    return dfsHeight (root) != -1;
+}
+
+   
