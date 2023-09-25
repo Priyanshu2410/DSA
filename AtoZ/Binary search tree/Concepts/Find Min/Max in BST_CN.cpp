acf95272b3697b80263b9800b801c8c2bc77fc13
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-09-25 23:06:17 +0530

    binary search tree CN

---
+++ AtoZ/Binary search tree/Concepts/Find Min/Max in BST_CN.cpp
@@ -0,0 +1,21 @@
+/*
+ * class Node
+ * {
+ * public:
+ *     int data;
+ *     Node *left;
+ *     Node *right;
+ *     Node() : data(0), left(nullptr), right(nullptr){};
+ *     Node(int x) : data(x), left(nullptr), right(nullptr) {}
+ *     Node(int x, Node *left, Node *right) : data(x), left(left), right(right) {}
+ * };
+ */
+int minVal(Node* root){
+
+    if(!root)return -1;
+
+    while(root->left)root=root->left;
+
+    return root->data;  
+
+}
