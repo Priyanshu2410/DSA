9265e63c1499476d52e2feb0b15d87a313ba8ca3
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-09-26 17:06:56 +0530

    binary search tree CN

---
+++ AtoZ/Binary search tree/Ceil from BST_CN.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure:
+
+    class BinaryTreeNode {
+    public:
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+        
+        BinaryTreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+        
+        ~BinaryTreeNode() {
+            if (left) {
+              delete left;
+            }
+            if (right) {
+              delete right;
+            }
+        }
+    };
+
+************************************************************/
+
+int findCeil(BinaryTreeNode<int> *node, int key){
+    int ceil = -1;
+    while(node)
+    {
+      if(node->data == key)
+      {
+        ceil = node->data;
+        return ceil;
+      }
+      if(key>node->data)
+      {
+        node = node->right;
+      }
+      else
+      {
+        ceil = node->data;
+        node = node->left;
+      }
+    }
+    return ceil;
+}
