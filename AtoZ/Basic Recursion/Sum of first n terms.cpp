a58408a484ff41ba1a2b96fce219b915be6f592c
Priyanshu Patel <95230102+Priyanshu2410@users.noreply.github.com>
2023-09-29 17:38:39 +0530

    Update Sum of first n terms.cpp

--- AtoZ/Basic Recursion/Sum of first n terms.cpp
+++ AtoZ/Basic Recursion/Sum of first n terms.cpp
@@ -10,11 +10,12 @@ class Solution {
   public:
     long long sumOfSeries(long long n) {
         // code here
-        if(n==0)
-        {
-            return 0;
-        }
-        return (n*n*n)+sumOfSeries(n-1);
+       if(n == 0)
+    {
+        return 0;
+    }
+
+    return n + sumFirstN(n-1);
     }
 };
 
