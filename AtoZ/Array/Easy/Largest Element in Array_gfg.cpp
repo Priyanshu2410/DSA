346547979b7a91b68cc0d9c973d94ab33cc74b39
Priyanshu Patel <95230102+Priyanshu2410@users.noreply.github.com>
2023-10-03 17:11:17 +0530

    Update Largest Element in Array_gfg.cpp

--- AtoZ/Array/Easy/Largest Element in Array_gfg.cpp
+++ AtoZ/Array/Easy/Largest Element in Array_gfg.cpp
@@ -19,6 +19,23 @@ public:
         }
         return ans;
     }
+
+    // other
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
+    // Write your code here.
+    int max = arr[0];
+    for(int i=0;i<arr.size();i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+        
+    }
+    return max;
+}
+
 };
 
 
