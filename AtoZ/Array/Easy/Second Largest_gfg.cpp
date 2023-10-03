40e449d17dfaeb665df0680ab5c6fb183c6531f6
Priyanshu Patel <95230102+Priyanshu2410@users.noreply.github.com>
2023-10-03 17:19:12 +0530

    Update Second Largest_gfg.cpp

--- AtoZ/Array/Easy/Second Largest_gfg.cpp
+++ AtoZ/Array/Easy/Second Largest_gfg.cpp
@@ -19,6 +19,20 @@ public:
          }
    return -1;
 	}
+
+	vector<int> getSecondOrderElements(int n, vector<int> a) {
+    // Write your code here.
+        sort(a.begin(), a.end());
+
+    vector<int> ans;
+
+    ans.push_back(a[n-2]);
+
+    ans.push_back(a[1]);
+
+    return ans;
+}
+
 };
 
 //{ Driver Code Starts.
