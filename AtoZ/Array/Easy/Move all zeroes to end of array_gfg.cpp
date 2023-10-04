6d18ac5569b8d17343ba5d30066818385680fac2
Priyanshu Patel <95230102+Priyanshu2410@users.noreply.github.com>
2023-10-04 10:08:55 +0530

    Update Move all zeroes to end of array_gfg.cpp

--- AtoZ/Array/Easy/Move all zeroes to end of array_gfg.cpp
+++ AtoZ/Array/Easy/Move all zeroes to end of array_gfg.cpp
@@ -21,6 +21,27 @@ public:
        }
 	    
 	}
+
+	// other
+	vector<int> moveZeros(int n, vector<int> a) {
+    // Write your code here.
+   int j=0;
+
+    for(int i=0;i<n;i++){
+
+        if(a[i]!=0){
+
+            swap(a[i],a[j]);
+
+            j++;
+
+        }
+
+    }
+
+    return a;
+}
+
 };
 
 //{ Driver Code Starts.
