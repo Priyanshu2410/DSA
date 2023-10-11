8b5002080b24b48cb00868b28c18296e4fa5cc50
Priyanshu Patel <95230102+Priyanshu2410@users.noreply.github.com>
2023-10-11 10:00:47 +0530

    Update Longest consecutive subsequence_gfg.cpp

--- AtoZ/Array/medium/Longest consecutive subsequence_gfg.cpp
+++ AtoZ/Array/medium/Longest consecutive subsequence_gfg.cpp
@@ -33,6 +33,35 @@ class Solution{
             
         return ans;
     }
+
+    // other
+    int longestSuccessiveElements(vector<int>&a) {
+    // Write your code here.
+    int n = a.size();
+    if (n == 0) return 0;
+
+    //sort the array:
+    sort(a.begin(), a.end());
+    int lastSmaller = INT_MIN;
+    int cnt = 0;
+    int longest = 1;
+
+    //find longest sequence:
+    for (int i = 0; i < n; i++) {
+        if (a[i] - 1 == lastSmaller) {
+            //a[i] is the next element of the
+            //current sequence.
+            cnt += 1;
+            lastSmaller = a[i];
+        }
+        else if (a[i] != lastSmaller) {
+            cnt = 1;
+            lastSmaller = a[i];
+        }
+        longest = max(longest, cnt);
+    }
+    return longest;
+}
 };
 
 //{ Driver Code Starts.
