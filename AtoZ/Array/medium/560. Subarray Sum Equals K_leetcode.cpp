20edce4a1469a0028afddd517406ef90be2dc4cb
Priyanshu Patel <95230102+Priyanshu2410@users.noreply.github.com>
2023-10-12 22:21:15 +0530

    Update 560. Subarray Sum Equals K_leetcode.cpp

--- AtoZ/Array/medium/560. Subarray Sum Equals K_leetcode.cpp
+++ AtoZ/Array/medium/560. Subarray Sum Equals K_leetcode.cpp
@@ -14,4 +14,25 @@ public:
     }
     return cnt;
     }
+
+    //other
+    int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    // Write Your Code Here
+    int n = arr.size();
+    int count =0;
+    for(int i=0;i<n;i++)
+    {
+        int sum =0;
+        for(int j=i;j<n;j++)
+        {
+            sum  += arr[j];
+            if(sum==k)
+            {
+                count++;
+            }
+        }
+        
+    }
+    return count;
+}
 };
