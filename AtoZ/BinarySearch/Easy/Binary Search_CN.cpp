c590ed74e00b6361346e7ea01a4b96f5df5bdb26
Priyanshu Patel <95230102+Priyanshu2410@users.noreply.github.com>
2023-10-13 22:00:55 +0530

    Update Binary Search_CN.cpp

--- AtoZ/BinarySearch/Easy/Binary Search_CN.cpp
+++ AtoZ/BinarySearch/Easy/Binary Search_CN.cpp
@@ -13,3 +13,16 @@ int search(vector<int>& nums, int target) {
     
      return binarySearch(nums, 0, nums.size() - 1, target);
 }
+
+
+ int n = nums.size(); //size of the array
+    int low = 0, high = n - 1;
+
+    // Perform the steps:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
